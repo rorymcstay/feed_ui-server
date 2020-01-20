f0b60466b67c79bcd289637aa932b18ab20bd4d3
Rory McStay <rory@rorymcstay.com>
2020-01-20 22:37:12 +0000

    test framework beginnings

---
+++ src/test/test_testutils.py
@@ -0,0 +1,54 @@
+import _sre
+import json
+from unittest import TestCase
+
+from flask import Response
+from flask_classy import route
+
+from src.test.testutils import DocumentationTest
+
+@route("...", methods=['GET', 'POST'])
+def getFeeds():
+    """
+    <example1>
+        #request=/getFeeds/
+        #response="["donedeal", "pistonheads"]
+    <example1/>
+
+    <example2>
+        #request: /getFeeds/
+        #response: ["donedeal", "pistonheads"]
+    <example2/>
+
+    :return:
+    """
+    pass
+
+
+class TestDocumentationTest(TestCase):
+    def test__test_case_regex(self):
+        regex = DocumentationTest._testCaseRegex('exampleName')
+        # self.assert_(isinstance(regex,  _sre.SRE_Pattern))
+        self.assert_(regex.pattern == '<exampleName>|<exampleName/>')
+
+    def test_get_valid_methods(self):
+        docTest = DocumentationTest(getFeeds)
+        mets = docTest.getValidMethods()
+        self.assert_(mets == ['GET', 'POST'])
+
+    def test_get_test_clauses(self):
+        docTest = DocumentationTest(getFeeds)
+        mets = docTest.getTestClauses('example1')
+        self.assert_(mets.get('request') is not None)
+        self.assert_(mets.get('response') is not None)
+
+    def test_get_names(self):
+        docTest = DocumentationTest(getFeeds)
+        names = docTest.getNames()
+        self.assert_('example1' in names)
+        self.assert_('example2' in names)
+
+    def test_generate(self):
+        er = DocumentationTest.generate(getFeeds)
+        self.assert_(er.cases.get('response') is not None)
+        self.assert_(er.cases.get('request') is not None)
