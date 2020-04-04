47e9dab9187874e3cd2cc3fc04d984fa396f6194
Rory McStay <rory@rorymcstay.com>
2020-04-05 00:09:49 +0100

    feed manager changes for status

--- ui-server.py
+++ ui-server.py
@@ -8,9 +8,7 @@ from feed.service import Client
 
 from src.main.feedmanager import FeedManager
 from src.main.scheduler import ScheduleManager
-from src.main.search import Search
 from src.main.tables import TableManager
-from src.main.mappermanager import MapperManager
 
 from feed.settings import *
 
@@ -21,7 +19,6 @@ app = Flask(__name__)
 logging.info("####### Environment #######")
 logging.info("mongo : {}".format(json.dumps(mongo_params, indent=4, sort_keys=True)))
 logging.info("kafka : {}".format(json.dumps(kafka_params, indent=4, sort_keys=True)))
-logging.info("hazelcast : {}".format(json.dumps(hazelcast_params, indent=4, sort_keys=True)))
 logging.info("database: {}".format(json.dumps(database_parameters, indent=4, sort_keys=True)))
 logging.info("feed : {}".format(json.dumps(feed_params, indent=4, sort_keys=True)))
 logging.info("persistence: {}".format(json.dumps(persistence_params, indent=4, sort_keys=True)))
@@ -30,7 +27,6 @@ logging.info("summarizer: {}".format(json.dumps(summarizer_params, indent=4, sor
 
 Client('commands', **command_params)
 CORS(app)
-Search.register(app)
 FeedManager.register(app )
 ScheduleManager.register(app )
 TableManager.register(app )
